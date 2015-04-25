63b03b563663e84fead613940292e14fb9bd33c5
chenqi <chenqi@taotaosou.com>
2015-04-25 14:54:55 +0800

    up config

--- config/develop.js
+++ config/develop.js
@@ -4,38 +4,20 @@ module.exports = {
 	port: 3003,
 	apiPrefix: '/api/v1',
 	log: {
-		name: 'm-chaoji99-service',
+		name: 'm-web-service',
 		logdir: __dirname + '/../../logs/',
 		duration: 86400000, // 1 day
 		nameformat: '{pid}-YYYY-MM-DD[.log]'
 	},
 	hosts: {
-		search: 'http://10.0.0.158:8099/s/',
 		ucService: 'http://uc.taotaosou.com',
-		appService: 'http://app.service.taotaosou.com',
-		findService: 'http://service1.taotaosou.com'
+		appService: 'http://app.service.taotaosou.com'
 	},
 	redis: {
 		port: 6379,
 		host: 'localhost'
 	},
 	mysql: {
-		five: {
-			host: '199.155.122.203',
-			port: '3306',
-			user: 'root',
-			password: '123456',
-			database: 'tts_taotaosou_five_haoxin',
-			connectionLimit: 5
-		},
-		find: {
-			host: '199.155.122.203',
-			port: '3306',
-			user: 'root',
-			password: '123456',
-			database: 'tts_taotaosou_find_crimson',
-			connectionLimit: 5
-		},
 		cms: {
 			host: '199.155.122.203',
 			port: '3306',
--- config/production.js
+++ config/production.js
@@ -4,43 +4,25 @@ module.exports = {
 	port: 3003,
 	apiPrefix: '/api/v1',
 	log: {
-		name: 'm-chaoji99-service',
+		name: 'm-web-service',
 		logdir: __dirname + '/../../logs/',
 		duration: 86400000, // 1 day
 		nameformat: '{pid}-YYYY-MM-DD[.log]'
 	},
 	hosts: {
-		search: 'http://10.0.0.158:8099/s/',
 		ucService: 'http://uc.taotaosou.com',
-		appService: 'http://app.service.taotaosou.com',
-		findService: 'http://service1.taotaosou.com'
+		appService: 'http://app.service.taotaosou.com'
 	},
 	redis: {
 		port: 6379,
-		host: 'localhost' // 192.168.3.21
+		host: 'localhost'
 	},
 	mysql: {
-		five: {
-			host: '10.0.0.134',
-			port: '3306',
-			user: 'vvlcardi',
-			password: 'VGO112a1ZBye',
-			database: 'tts_taotaosou_five',
-			connectionLimit: 5
-		},
-		find: {
-			host: '10.0.0.134',
-			port: '3306',
-			user: 'vvlcardi',
-			password: 'VGO112a1ZBye',
-			database: 'tts_taotaosou_find',
-			connectionLimit: 5
-		},
 		cms: {
-			host: '10.0.0.134',
+			host: '199.155.122.203',
 			port: '3306',
-			user: 'vvlcardi',
-			password: 'VGO112a1ZBye',
+			user: 'root',
+			password: '123456',
 			database: 'tts_taotaosou_cms',
 			connectionLimit: 5
 		}
